bfe4e764f8ae4c293ad7b1970e486db10c677011
infor.ljh <infor.ljh.com>
2024-05-13 15:04:00 +0900

    1. ci/cd 구축 시작

--- src/main/java/com/example/testcicd/controller/TestController.java
+++ src/main/java/com/example/testcicd/controller/TestController.java
@@ -2,23 +2,22 @@ package com.example.testcicd.controller;
 
 import com.example.testcicd.TestCicdApplication;
 import com.example.testcicd.service.TestService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor // final을 붙인 레이어에 자동으로 의존성 주입.
 public class TestController {
 
-    private TestService testService;
+    private final TestService testService;
 
-    @Autowired
-    public TestController(TestService testService){
-        this.testService = testService;
-    }
 
-    @GetMapping
-    public String test(){
+    @GetMapping("/test")
+    public String getTestString() {
         return testService.getTestString();
     }
+
 }
