fc20e8b138ef607c80caae713299058015d7909b
infor.ljh <infor.ljh.com>
2024-05-13 15:05:11 +0900

    1. ci/cd 구축 시작

---
+++ src/test/java/com/example/testcicd/service/TestServicelmpTest.java
@@ -0,0 +1,42 @@
+package com.example.testcicd.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("dev")
+class TestServiceImplTest {
+
+    @Autowired private TestServicelmpl testService;
+
+    @Value("${server.port}")
+    private String port;
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Test
+    void checkPortSuccess(){
+        assertEquals("3444", port);
+    }
+
+    @Test
+    void checkPortFail(){
+        assertNotEquals("8080", port);
+    }
+
+    @Test
+    void getTestStringSuccess() {
+        assertEquals("okay " + applicationName + " " + port, testService.getTestString());
+    }
+
+    @Test
+    void getTestStringFail() {
+        assertNotEquals("okay Test FAIL", testService.getTestString());
+    }
+}
